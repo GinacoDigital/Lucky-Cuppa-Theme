fe9d136ca6eb7346d9b46ca43f40e9d4fae14a9f
BirikyC <adam.ceglarz18@gmail.com>
2023-06-30 00:46:33 +0200

    default template
    Default html template for archives and pages

--- page.php
+++ page.php
@@ -1,1 +1,10 @@
-test123
+<?php get_header(); ?>
+<section class="container">
+  <div class="page-section__title">
+    <span class="font-heading"><?php the_title(); ?></span>
+  </div>
+  <div class="page-section__content">
+    <span class="font-body"><?php the_content(); ?></span>
+  </div>
+</section>
+<?php get_footer(); ?>
