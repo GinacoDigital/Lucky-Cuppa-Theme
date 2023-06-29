908989f67c4e9ffdd99323662c973d6fa4c6c958
BirikyC <adam.ceglarz18@gmail.com>
2023-06-30 01:08:49 +0200

    page styling

--- page.php
+++ page.php
@@ -1,10 +1,12 @@
 <?php get_header(); ?>
 <section class="container">
-  <div class="page-section__title">
-    <span class="font-heading"><?php the_title(); ?></span>
-  </div>
-  <div class="page-section__content">
-    <span class="font-body"><?php the_content(); ?></span>
+  <div class="page-section">
+    <div class="page-section__title">
+      <span class="font-heading"><?php the_title(); ?></span>
+    </div>
+    <div class="page-section__content">
+      <span class="font-subtitle"><?php the_content(); ?></span>
+    </div>
   </div>
 </section>
 <?php get_footer(); ?>
